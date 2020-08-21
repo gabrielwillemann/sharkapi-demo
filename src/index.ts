1ec91e2c0e5184b8474cf09ec14d79876f97f0f3
Gabriel Willemann <gabrielw@outlook.com>
2020-08-21 17:05:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Sequelize, DataTypes } from 'sequelize';
+import * as sharkApi from 'sharkapi';
+
+let app;
+let sequelize;
+
+(async () => {
+  await startSequelize();
+  await startExpress();
+  await startSharkApi();
+})();
+
+async function startSequelize() {
+  try {
+    sequelize = new Sequelize({ dialect: 'sqlite', storage: 'database.sqlite' });
+    await sequelize.authenticate();
+    sequelize.define('Person', { name: DataTypes.STRING, age: DataTypes.INTEGER }, { underscored: true });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function startExpress() {
+  app = express();
+  app.use(bodyParser.json());
+
+  // app.get('/people', async (req, res) => {
+  //   let people = await sequelize.models.Person.findAll();
+  //   res.send(people);
+  // });
+
+  // app.post('/people', async (req, res) => {
+  //   let people = await sequelize.models.Person.create(req.body);
+  //   res.send(people);
+  // });
+
+  let port = process.argv[2] || 3000;
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+  });
+}
+
+async function startSharkApi() {
+  sharkApi.express(app);
+  sharkApi.sequelizeModel(sequelize.models.Person);
+}
