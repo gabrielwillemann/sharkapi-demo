a79b8f4560b22c32004430e6760308fd883e56bb
Gabriel Willemann <gabrielw@outlook.com>
2020-08-28 17:03:31 -0300

    Add example for hooks feature

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,15 @@ async function startSequelize() {
   try {
     sequelize = new Sequelize({ dialect: 'sqlite', storage: 'database.sqlite' });
     await sequelize.authenticate();
-    sequelize.define('Person', { name: DataTypes.STRING, age: DataTypes.INTEGER }, { underscored: true });
+    let City = sequelize.define('City', { name: DataTypes.STRING }, { underscored: true });
+    let Person = sequelize.define('Person', { name: DataTypes.STRING, age: DataTypes.INTEGER }, { underscored: true });
+    let Car = sequelize.define('Car', { name: DataTypes.STRING }, { underscored: true });
+
+    Person.belongsTo(City);
+    City.hasMany(Person);
+
+    Car.belongsTo(Person);
+    Person.hasMany(Car);
   } catch (error) {
     console.log(error);
   }
@@ -25,16 +33,6 @@ async function startSequelize() {
 async function startExpress() {
   app = express();
   app.use(bodyParser.json());
-
-  // app.get('/people', async (req, res) => {
-  //   let people = await sequelize.models.Person.findAll();
-  //   res.send(people);
-  // });
-
-  // app.post('/people', async (req, res) => {
-  //   let people = await sequelize.models.Person.create(req.body);
-  //   res.send(people);
-  // });
 
   let port = process.argv[2] || 3000;
   app.listen(port, () => {
@@ -45,6 +43,25 @@ async function startExpress() {
 async function startSharkApi() {
   let sharkApi = new SharkApi();
   sharkApi.restApiServer(app);
+  sharkApi.entitySequelize(sequelize.models.City);
   sharkApi.entitySequelize(sequelize.models.Person);
+  sharkApi.entitySequelize(sequelize.models.Car);
+
+  // sharkApi.entitySequelize(sequelize.models.City, {
+  //   hooks: [
+  //     {
+  //       triggered: 'filter', // all, filter, relationship, sort ...
+  //       id: 'name-like',
+  //       fn({entity, context, query, value, req, hookId}) {
+  //         // context in sequelize is object passed as parameter, but in TypeOrm and Mogoose is QueryBuild
+  //         // Take care for clear all data generate by SharkApi
+  //         context.where = context.where || {};
+  //         context.include = context.include || {};
+  //         return context;
+  //       }
+  //     }
+  //   ]
+  // });
+
   sharkApi.createResources();
 }
