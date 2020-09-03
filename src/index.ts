46ff875de81c3db18bd8723d183fbe7f966e760c
Gabriel Willemann <gabrielw@outlook.com>
2020-09-03 18:25:45 -0300

    Design and test pagination hook

--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,28 @@ async function startSequelize() {
   try {
     sequelize = new Sequelize({ dialect: 'sqlite', storage: 'database.sqlite' });
     await sequelize.authenticate();
+
     let City = sequelize.define('City', { name: DataTypes.STRING }, { underscored: true });
     let Person = sequelize.define('Person', { name: DataTypes.STRING, age: DataTypes.INTEGER }, { underscored: true });
     let Car = sequelize.define('Car', { name: DataTypes.STRING }, { underscored: true });
+    let Color = sequelize.define('Color', { name: DataTypes.STRING, hexCode: DataTypes.STRING }, { underscored: true });
 
     Person.belongsTo(City);
     City.hasMany(Person);
 
     Car.belongsTo(Person);
     Person.hasMany(Car);
+
+    Car.belongsTo(Color);
+    Color.hasMany(Car);
+
+    // await City.create({ name: 'Paramus' });
+    // await Person.create({ name: 'Andew' });
+    // await Color.create({ name: 'Silver', hexCode: 'c5ced4' });
+    // await Car.create({ name: 'Toyota Avalon' });
+
+    // await Color.sync({ force: true });
+    // await Car.sync({ force: true });
   } catch (error) {
     console.log(error);
   }
@@ -44,74 +57,79 @@ async function startSharkApi() {
   let sharkApi = new SharkApi();
   sharkApi.restApiServer(app);
   sharkApi.entitySequelize(sequelize.models.City);
-  sharkApi.entitySequelize(sequelize.models.Person);
-  sharkApi.entitySequelize(sequelize.models.Car, {
+  sharkApi.entitySequelize(sequelize.models.Person, {
     hooks: [
       {
-        trigger: 'sort',
-        match: 'person_name',
+        trigger: 'page',
+        match: 'limit',
         fn({ context, name, value }) {
-          context.order = context.order || [];
-          context.order.push([sequelize.models.Person, 'name', value]);
-          context.include = context.include || [];
-          context.include.push({ model: sequelize.models.Person });
-          return context;
-        },
-      },
-      {
-        trigger: 'filter',
-        match: 'person_name',
-        fn({ context, name, value }) {
-          context.include = context.include || [];
-          context.include.push({
-            model: sequelize.models.Person,
-            where: {
-              name: { [Op.like]: `%${value}%` },
-            },
-          });
-          return context;
-        },
-      },
-      {
-        trigger: 'relationship',
-        match: 'person_with_city',
-        fn({ context, name, value }) {
-          context.include = context.include || [];
-          context.include.push({
-            model: sequelize.models.Person,
-            include: [{ model: sequelize.models.City }],
-          });
-          return context;
+          context.limit = parseInt(value);
+          // context.offset = ;
         },
       },
     ],
   });
 
+  sharkApi.entitySequelize(sequelize.models.Color);
+
+  sharkApi.entitySequelize(sequelize.models.Car, {
+    hooks: [
+      // {
+      //   trigger: 'page',
+      //   match: 'limit',
+      //   fn({ context, name, value }) {
+      //     console.log('aaaaaaaaaaaaa', context, name, value);
+      //   },
+      // },
+      // {
+      //   trigger: 'page',
+      //   match: 'offset',
+      //   fn() {},
+      // },
+      // {
+      //   trigger: 'page',
+      //   match: 'new-limit',
+      //   fn() {},
+      // },
+      // {
+      //   trigger: 'filter',
+      //   match: 'person_name',
+      //   fn({ context, name, value }) {
+      //     context.include = context.include || [];
+      //     context.include.push({
+      //       model: sequelize.models.Person,
+      //       where: { name: { [Op.like]: `%${value}%` } },
+      //     });
+      //     return context;
+      //   },
+      // },
+      // {
+      //   trigger: 'sort',
+      //   match: 'person_name',
+      //   fn({ context, name, value }) {
+      //     context.order = context.order || [];
+      //     context.order.push([sequelize.models.Person, 'name', value]);
+      //     context.include = context.include || [];
+      //     context.include.push({ model: sequelize.models.Person });
+      //     return context;
+      //   },
+      // },
+      // {
+      //   trigger: 'relationship',
+      //   match: 'person_with_city',
+      //   fn({ context, name, value }) {
+      //     context.include = context.include || [];
+      //     context.include.push({
+      //       model: sequelize.models.Person,
+      //       include: [{ model: sequelize.models.City }],
+      //     });
+      //     return context;
+      //   },
+      // },
+    ],
+  });
+
   sharkApi.createResources();
-
-  // sharkApi.entitySequelize(sequelize.models.City, {
-  //   hooks: [
-  //     {
-
-  //       // valid options: index-before, index-after, create-before, create-after, update-{}, delete-{}
-  //       // valid options: filter, relationship, sort, page
-  //       trigger: 'filter',
-
-  //       // only valid for: filter, relationship, sort, page
-  //       // string or regex
-  //       match: 'name-like',
-
-  //       // this function will be called by entity with Context = Query, and will receive return
-  //       fn({entity, context, value, triggered, name/match, req?}) {
-  //         // context in sequelize is object passed as parameter, but in TypeOrm and Mogoose is QueryBuild
-  //         // Take care for clear all data generate by SharkApi
-  //         context.where = context.where || {};
-  //         context.include = context.include || {};
-  //         return context;
-  //       }
-  //     }
-  //   ]
-  // });
 
   // global hooks, for all entities
   // sharkApi.hooks = [{}];
