a535007af589f6ed70f2033472d28259195c0d3a
Gabriel Willemann <gabrielw@outlook.com>
2020-09-03 13:44:12 -0300

    Design and test Hooks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize, DataTypes, Op } from 'sequelize';
 import { SharkApi } from 'sharkapi';
 
 let app;
@@ -45,14 +45,64 @@ async function startSharkApi() {
   sharkApi.restApiServer(app);
   sharkApi.entitySequelize(sequelize.models.City);
   sharkApi.entitySequelize(sequelize.models.Person);
-  sharkApi.entitySequelize(sequelize.models.Car);
+  sharkApi.entitySequelize(sequelize.models.Car, {
+    hooks: [
+      {
+        trigger: 'sort',
+        match: 'person_name',
+        fn({ context, name, value }) {
+          context.order = context.order || [];
+          context.order.push([sequelize.models.Person, 'name', value]);
+          context.include = context.include || [];
+          context.include.push({ model: sequelize.models.Person });
+          return context;
+        },
+      },
+      {
+        trigger: 'filter',
+        match: 'person_name',
+        fn({ context, name, value }) {
+          context.include = context.include || [];
+          context.include.push({
+            model: sequelize.models.Person,
+            where: {
+              name: { [Op.like]: `%${value}%` },
+            },
+          });
+          return context;
+        },
+      },
+      {
+        trigger: 'relationship',
+        match: 'person_with_city',
+        fn({ context, name, value }) {
+          context.include = context.include || [];
+          context.include.push({
+            model: sequelize.models.Person,
+            include: [{ model: sequelize.models.City }],
+          });
+          return context;
+        },
+      },
+    ],
+  });
+
+  sharkApi.createResources();
 
   // sharkApi.entitySequelize(sequelize.models.City, {
   //   hooks: [
   //     {
-  //       triggered: 'filter', // all, filter, relationship, sort ...
-  //       id: 'name-like',
-  //       fn({entity, context, query, value, req, hookId}) {
+
+  //       // valid options: index-before, index-after, create-before, create-after, update-{}, delete-{}
+  //       // valid options: filter, relationship, sort, page
+  //       trigger: 'filter',
+
+  //       // only valid for: filter, relationship, sort, page
+  //       // string or regex
+  //       match: 'name-like',
+
+  //       // this function will be called by entity with Context = Query, and will receive return
+  //       fn({entity, context, value, triggered, name/match, req?}) {
   //         // context in sequelize is object passed as parameter, but in TypeOrm and Mogoose is QueryBuild
   //         // Take care for clear all data generate by SharkApi
   //         context.where = context.where || {};
@@ -63,5 +113,6 @@ async function startSharkApi() {
   //   ]
   // });
 
-  sharkApi.createResources();
+  // global hooks, for all entities
+  // sharkApi.hooks = [{}];
 }
